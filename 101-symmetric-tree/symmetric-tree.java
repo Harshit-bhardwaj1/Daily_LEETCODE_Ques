6c6a0234711fa0d42c02ea2198fc24edc4732fdd
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-09-25 11:10:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (6.94%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -20,30 +20,52 @@
 // }
 
 
+// class Solution {
+//     public boolean isSymmetric(TreeNode root) {
+//         if (root == null) return true;
+
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.add(root.left);
+//         queue.add(root.right);
+
+//         while (!queue.isEmpty()) {
+//             TreeNode left = queue.poll();
+//             TreeNode right = queue.poll();
+
+//             if (left == null && right == null) continue;
+//             if (left == null || right == null) return false;
+//             if (left.val != right.val) return false;
+
+//             // Add opposite children to check mirror property
+//             queue.add(left.left);
+//             queue.add(right.right);
+
+//             queue.add(left.right);
+//             queue.add(right.left);
+//         }
+
+//         return true;
+//     }
+// }
+
+
 class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        if (root == null) return true;
+    public boolean isSymmetric(TreeNode root){
+        return check(root.left,root.right);
+    }
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root.left);
-        queue.add(root.right);
-
-        while (!queue.isEmpty()) {
-            TreeNode left = queue.poll();
-            TreeNode right = queue.poll();
-
-            if (left == null && right == null) continue;
-            if (left == null || right == null) return false;
-            if (left.val != right.val) return false;
-
-            // Add opposite children to check mirror property
-            queue.add(left.left);
-            queue.add(right.right);
-
-            queue.add(left.right);
-            queue.add(right.left);
+    public boolean check(TreeNode n1, TreeNode n2){
+        if(n1==null && n2==null){
+            return true;
         }
-
-        return true;
+        if(n1==null || n2==null){
+            return false;
+        }
+        if(n1.val!=n2.val){
+            return false;
+        }
+        boolean left = check(n1.left,n2.right);
+        boolean right = check(n1.right, n2.left);
+        return left && right;
     }
 }
