f3f3a6a79a527c18eff6b008c2276e0387dfe3d8
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-09-26 23:54:03 +0530

    Time: 105 ms (31.02%) | Memory: 49.2 MB (69.86%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,51 @@
+// class Solution {
+//     public int countPrimes(int n) {
+//         if(n<=2){
+//             return 0;
+//         }
+//         return PrimeSieve(n);
+//     }
+//     public static int PrimeSieve(int n){
+//         int[] ans= new int[n];
+//         ans[0]=ans[1]=1;
+//         for(int i=2; i*i<=ans.length; i++){
+//             if(ans[i]==0){ // i prime hai
+//                 for(int j=2; i*j<ans.length; j++){
+//                     ans[i*j]=1;
+//                 }
+//             }
+//         }
+//         int c=0;
+//         for(int i=2; i<ans.length; i++){
+//             if(ans[i]==0){
+//                 c++;
+//             }
+//         }
+//         return c;
+//     }
+// }
+
+
+class Solution{
+    public int countPrimes(int n){
+        if(n<2) return 0;
+        return PrimeSeive(n);
+    }
+    public int PrimeSeive(int n){
+        boolean [] arr = new boolean[n];
+        Arrays.fill(arr,true);
+        arr[0]=arr[1]=false;
+        for(int i=0; i*i<=n; i++){
+            if(arr[i]){
+                for(int j=2; i*j<n; j++){
+                    arr[i*j]=false;
+                }
+            }
+        }
+        int count=0;
+        for(boolean x:arr){
+            if(x) count++;
+        }
+        return count;
+    }
+}
