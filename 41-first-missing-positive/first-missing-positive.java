a846537bce1226d4616f5986c1bcc3fe8267ef05
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-09-27 15:47:40 +0530

    Time: 1 ms (100.00%) | Memory: 56.9 MB (77.18%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int firstMissingPositive(int[] arr) {
+        boolean flag = false;
+        int n= arr.length;
+
+        for(int i=0; i<n; i++){
+            if(arr[i]==1){
+                flag= true;
+            }
+            if(arr[i]<1 || arr[i]>n){
+                arr[i]=1;
+            }
+        }
+        if(flag==false){
+            return 1;
+        }
+
+        for(int i=0; i<n; i++){
+            int idx=Math.abs(arr[i])-1;
+            if(arr[idx] > 0) arr[idx] *= -1;
+            // arr[idx]= Math.abs(arr[i]) * -1;   
+        }
+
+        for(int i=0; i<n; i++){
+            if(arr[i]>0){
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+}
