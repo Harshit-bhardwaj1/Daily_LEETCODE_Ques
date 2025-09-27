497927a085a5d25a546ae9c3d696460d7ebb6560
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-09-27 13:28:22 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (79.07%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,46 @@
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         return Search(nums,target);
+//     }
+//     public static int Search(int[] arr, int target){
+//         int lo=0;
+//         int hi=arr.length-1;
+//         while(lo<=hi){
+//             int mid= lo+(hi-lo)/2;
+//             if(arr[mid]==target){
+//                 return mid;
+//             }
+//             else if(arr[mid]<target){
+//                 lo=mid+1;
+//             }
+//             else{
+//                 hi=mid-1;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+class Solution{
+    public int search(int[] nums, int target){
+        return Search(nums,target);
+    }
+    public int Search(int[] arr, int target){
+        int lo=0;
+        int hi=arr.length-1;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]>target){
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return -1;
+    }
+}
