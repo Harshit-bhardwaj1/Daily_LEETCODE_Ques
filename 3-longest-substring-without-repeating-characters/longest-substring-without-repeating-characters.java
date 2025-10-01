875157cc405f60d36f97a569a0222a5de8dfcdbc
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-10-01 18:29:12 +0530

    Time: 2 ms (98.85%) | Memory: 43.9 MB (87.63%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -0,0 +1,70 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        return Print(s);
+
+    }
+    
+//     public int Print(String s) {
+//     int si = 0; // Start index of the sliding window
+//     int ei = 0; // End index of the sliding window
+//     int ans = 0; // Maximum length of substring without repeating characters
+
+//     int[] freq = new int[256]; // Frequency array for characters (ASCII size)
+
+//     while (ei < s.length()) {
+//         char currentChar = s.charAt(ei);
+//         freq[currentChar]++;
+
+//         // Shrink the window if the character is repeated
+//         while (freq[currentChar] > 1) {
+//             freq[s.charAt(si)]--;
+//             si++;
+//         }
+
+//         // Update the maximum length
+//         ans = Math.max(ans, ei - si + 1);
+//         ei++;
+//     }
+
+//     return ans;
+// }
+
+    // public int Print(String s){
+    //     int si=0; 
+    //     int ei=0;
+    //     int ans=0;
+    //     int[] arr= new int[256];
+    //     while(ei<s.length()){
+    //         arr[s.charAt(ei)]++;
+
+    //         while(arr[s.charAt(ei)]>1){
+    //             arr[s.charAt(si)]--;
+    //             si++;
+    //         }
+    //         ans= Math.max(ans,ei-si+1);
+    //         ei++;
+    //     }
+    //     return ans;
+    // }
+
+
+    public int Print(String s){
+        int[] freq=new int[256];
+        int ans=0;
+        int ei=0;
+        int si=0;
+        while(ei<s.length()){
+            char ch= s.charAt(ei);
+            freq[ch]++;
+
+            while(freq[ch]>1){
+                freq[s.charAt(si)]--;
+                si++;
+            }
+
+            ans=Math.max(ans,ei-si+1);
+            ei++;
+        }
+        return ans;
+    }
+}
