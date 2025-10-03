280259484360ddfeeac5a7d86b0ab652dc5e0553
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-10-03 11:04:45 +0530

    Time: 2 ms (44.35%) | Memory: 43.5 MB (24.52%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,21 +1,42 @@
-class Solution {
-    public List<String> generateParenthesis(int n) {
+// class Solution {
+//     public List<String> generateParenthesis(int n) {
+//         List<String> ll = new ArrayList<>();
+//         Parenthesis(n, 0, 0, "",ll);
+//         return ll;
+//     }
+//     public static void Parenthesis(int n, int closed, int open, String ans, List<String> ll){
+//         if(open==n && closed==n){
+//             //System.out.println(ans);
+
+//             ll.add(ans);
+//             return;
+//         }
+//         if(open>n || closed > open){
+//             return;
+//         }
+
+//         Parenthesis(n, closed, open+1, ans+"(",ll);
+//         Parenthesis(n, closed+1, open, ans+")",ll);
+//     }
+// }
+
+
+class Solution{
+    public List<String> generateParenthesis(int n){
         List<String> ll = new ArrayList<>();
-        Parenthesis(n, 0, 0, "",ll);
+        Generate(ll,0,0,"",n);
         return ll;
     }
-    public static void Parenthesis(int n, int closed, int open, String ans, List<String> ll){
-        if(open==n && closed==n){
-            //System.out.println(ans);
-
+    public void Generate(List<String> ll , int closed, int open, String ans,int n){
+        if(closed==n && open== n){
             ll.add(ans);
             return;
         }
-        if(open>n || closed > open){
+        if(open>n || closed >open){
             return;
         }
 
-        Parenthesis(n, closed, open+1, ans+"(",ll);
-        Parenthesis(n, closed+1, open, ans+")",ll);
+        Generate(ll,closed,open+1,ans+"(",n);
+        Generate(ll,closed+1,open,ans+")",n);
     }
 }
