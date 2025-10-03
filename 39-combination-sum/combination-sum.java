439209cd3887ffdc4afd6d1172d7196268febc20
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-10-03 11:30:22 +0530

    Time: 1 ms (99.88%) | Memory: 44.6 MB (64.69%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,47 +1,21 @@
-// class Solution {
-//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-//         List<List<Integer>> ans= new ArrayList<>();
-//         List<Integer> ll = new ArrayList<>();
-//         Combination(candidates, target, ll, 0,ans);
-//         return ans;
-//     }
-//     public static void Combination(int[] coin, int amount,List<Integer> ll,int idx, List<List<Integer>> ans){
-//         if(amount==0){
-//             //System.out.println(ll);
-//             ans.add(new ArrayList<>(ll));
-//             return;
-//         }
-//         for(int i=idx; i<coin.length; i++){
-//             if(amount>=coin[i]){
-//                 ll.add(coin[i]);
-//                 Combination(coin, amount-coin[i], ll,i,ans);
-//                 ll.remove(ll.size()-1);
-//             }
-//         }
-//     }
-// }
-
-
-class Solution{
-    public List<List<Integer>> combinationSum(int[] candidates, int target){
-        List<List<Integer>> ll = new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
-        Sum(ll,candidates,target,0,ans,0);
-        return ll;
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans= new ArrayList<>();
+        List<Integer> ll = new ArrayList<>();
+        Combination(candidates, target, ll, 0,ans);
+        return ans;
     }
-    public void Sum(List<List<Integer>> ll, int[] candidates, int target, int amount,List<Integer> ans,int idx){
-        if(amount==target){
-            ll.add(new ArrayList<>(ans));
+    public static void Combination(int[] coin, int amount,List<Integer> ll,int idx, List<List<Integer>> ans){
+        if(amount==0){
+            //System.out.println(ll);
+            ans.add(new ArrayList<>(ll));
             return;
         }
-        for(int i= idx; i<candidates.length; i++){
-            if(amount<target){
-
-                amount=amount+candidates[i];
-                ans.add(candidates[i]);
-                Sum(ll,candidates,target,amount,ans,i);
-                amount=amount-candidates[i];
-                ans.remove(ans.size()-1);
+        for(int i=idx; i<coin.length; i++){
+            if(amount>=coin[i]){
+                ll.add(coin[i]);
+                Combination(coin, amount-coin[i], ll,i,ans);
+                ll.remove(ll.size()-1);
             }
         }
     }
