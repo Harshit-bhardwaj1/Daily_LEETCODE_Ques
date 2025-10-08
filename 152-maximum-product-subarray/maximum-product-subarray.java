e95d1588b9fdabe6f13f95f336944e8c6c8b2c72
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-10-08 11:38:14 +0530

    Time: 2 ms (55.14%) | Memory: 44.9 MB (74.46%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,32 +1,53 @@
+// class Solution {
+//     public int maxProduct(int[] nums) {
+//         return Product(nums);
+//     }
+//     public int Product(int[] nums){
+//         int product = 1;
+//         int maxProduct = Integer.MIN_VALUE;
+
+//         int si = 0, ei = 0;
+//         while (ei < nums.length) {
+//             product *= nums[ei];
+//             maxProduct = Math.max(maxProduct, product);
+
+//             // if product becomes 0, reset window
+//             if (product == 0) {
+//                 product = 1;
+//                 si = ei + 1;
+//             }
+//             ei++;
+//         }
+
+//         // handle negatives by doing another pass from right to left
+//         product = 1;
+//         for (int i = nums.length - 1; i >= 0; i--) {
+//             product *= nums[i];
+//             maxProduct = Math.max(maxProduct, product);
+//             if (product == 0) product = 1;
+//         }
+
+//         return maxProduct;
+//     }
+// }
+
 class Solution {
     public int maxProduct(int[] nums) {
-        return Product(nums);
-    }
-    public int Product(int[] nums){
-        int product = 1;
-        int maxProduct = Integer.MIN_VALUE;
+        int n = nums.length;
+        int maxHere = nums[0];
+        int minHere = nums[0]; // track min because negatives can flip
+        int result = nums[0];
+        
+        for (int i = 1; i < n; i++) {
+            int temp = maxHere;
 
-        int si = 0, ei = 0;
-        while (ei < nums.length) {
-            product *= nums[ei];
-            maxProduct = Math.max(maxProduct, product);
+            // choose between current number alone or extending product
+            maxHere = Math.max(nums[i], Math.max(maxHere * nums[i], minHere * nums[i]));
+            minHere = Math.min(nums[i], Math.min(temp * nums[i], minHere * nums[i]));
 
-            // if product becomes 0, reset window
-            if (product == 0) {
-                product = 1;
-                si = ei + 1;
-            }
-            ei++;
+            result = Math.max(result, maxHere);
         }
-
-        // handle negatives by doing another pass from right to left
-        product = 1;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            product *= nums[i];
-            maxProduct = Math.max(maxProduct, product);
-            if (product == 0) product = 1;
-        }
-
-        return maxProduct;
+        
+        return result;
     }
 }
