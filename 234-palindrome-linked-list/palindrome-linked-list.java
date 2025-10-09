06d44b9f30d3285835355a92da35cff22472ae06
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-10-09 15:18:42 +0530

    Time: 4 ms (83.07%) | Memory: 59.8 MB (70.73%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -11,12 +11,14 @@
 class Solution {
     ListNode curr;
     public boolean isPalindrome(ListNode head) {
-        if(head==null) return false;
-        curr=head;
+        if(head==null || head.next==null) return true;
         List<Integer> ll = new ArrayList<>();
         // return Palindrome1(head,ll);
         // return Palindrome2(head);
-        return Palindrome3(head);
+        curr=head;
+        // return Palindrome3(head);
+        return Palindrome4(head);
+        
 
     }
     // Approach 1 Arrays List
@@ -37,6 +39,7 @@ class Solution {
         return true;
     }
 
+    // Approach 2 stack
     public boolean Palindrome2(ListNode head){
         Stack<Integer> st = new Stack<>();
         ListNode temp=head;
@@ -52,12 +55,46 @@ class Solution {
         }
         return true;
     }
-
+    
+    // Approach 3 recursion
     public boolean Palindrome3(ListNode head){
         if(head==null) return true;
 
         boolean ans = Palindrome3(head.next) && head.val == curr.val;
         curr= curr.next;
         return ans;
+    }
+
+    // Approach 4 half pointer
+    public boolean Palindrome4(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast= fast.next.next;
+        }
+
+        ListNode firsthalf = head;
+        ListNode secondhalf= reverse(slow);
+
+        while(secondhalf!=null){
+            if(secondhalf.val!=firsthalf.val){
+                return false;
+            }
+            firsthalf=firsthalf.next;
+            secondhalf = secondhalf.next;
+        }
+        return true;
+    }
+    public ListNode reverse(ListNode head){
+        ListNode prev= null;
+        ListNode curr1 = head;
+        while(curr1!=null){
+            ListNode next = curr1.next;
+            curr1.next=prev;
+            prev=curr1;
+            curr1=next;
+        }
+        return prev;
     }
 }
