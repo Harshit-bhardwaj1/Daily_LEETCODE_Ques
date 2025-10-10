425f809b6bcc5fbfe2de89666df1ff11057509a7
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-10-10 10:02:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (38.09%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,20 +1,42 @@
-class Solution {
-    public int minCostClimbingStairs(int[] cost) {
+// class Solution {
+//     public int minCostClimbingStairs(int[] cost) {
+//         int[] dp = new int[cost.length];
+//         Arrays.fill(dp,-1);
+//         int zeroth = Cost(cost,0,dp);
+//         int first =  Cost(cost,1,dp);
+//         return  Math.min(zeroth,first); 
+//           }
+//     public int Cost(int[] cost,int i, int[] dp){
+//         if(i>=cost.length){
+//             return 0;
+//         }
+//         if(dp[i]!=-1){
+//             return dp[i];
+//         }
+//         int step1= cost[i]+Cost(cost,i+2,dp);
+//         int step0= cost[i]+Cost(cost,i+1,dp);
+//         return dp[i]= Math.min(step1,step0);
+//     }
+// }
+
+
+class Solution{
+    public int minCostClimbingStairs(int[] cost){
         int[] dp = new int[cost.length];
         Arrays.fill(dp,-1);
-        int zeroth = Cost(cost,0,dp);
-        int first =  Cost(cost,1,dp);
-        return  Math.min(zeroth,first); 
-          }
-    public int Cost(int[] cost,int i, int[] dp){
-        if(i>=cost.length){
+        int zero =Cost(cost,dp,0);
+        int one =Cost(cost,dp,1);
+        return Math.min(zero,one);
+    }
+    public int Cost(int[] cost,int[] dp, int idx){
+        if(idx>=cost.length){
             return 0;
         }
-        if(dp[i]!=-1){
-            return dp[i];
+        if(dp[idx]!=-1){
+            return dp[idx];
         }
-        int step1= cost[i]+Cost(cost,i+2,dp);
-        int step0= cost[i]+Cost(cost,i+1,dp);
-        return dp[i]= Math.min(step1,step0);
+        int step1= Cost(cost,dp,idx+1);
+        int step2= Cost(cost,dp,idx+2);
+        return dp[idx]= cost[idx] + Math.min(step1,step2);
     }
 }
