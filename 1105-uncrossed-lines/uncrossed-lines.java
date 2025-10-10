3be44162cfa4a084ba6e6c681dcfc45a2ca5b812
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-10-10 17:28:55 +0530

    Time: 7 ms (24.26%) | Memory: 44.8 MB (5.57%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.java
+++ 1105-uncrossed-lines/uncrossed-lines.java
@@ -1,6 +1,12 @@
 class Solution {
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
-        return LCSBU(nums1,nums2);
+        int[][] dp = new int[nums1.length+1][nums2.length+1];
+        for(int[] a : dp){
+            Arrays.fill(a,-1);
+        }
+        // return LCSBU(nums1,nums2);
+        return LCSTD(nums1,nums2,0,0,dp);
+
     }
     public static int LCSBU(int []s1, int []s2){
         int[][] dp = new int[s1.length+1][s2.length+1];
@@ -17,5 +23,23 @@ class Solution {
             }
         }
         return dp[dp.length - 1][dp[0].length-1];
+    }
+
+    public static int LCSTD(int[] s1, int[] s2, int i, int j, int[][] dp){
+        if(i==s1.length || j==s2.length){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s1[i]==s2[j]){
+            dp[i][j]=1+ LCSTD(s1,s2,i+1,j+1,dp);
+        }
+        else{
+            int f= LCSTD(s1,s2,i+1,j,dp);
+            int s= LCSTD(s1,s2,i,j+1,dp);
+            dp[i][j]=Math.max(f,s);
+        }
+        return dp[i][j];
     }
 }
