962e3db49c0d251eb8f998e16687866e7370c66a
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-10-10 16:39:40 +0530

    Time: 21 ms (52.85%) | Memory: 57.5 MB (5.02%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -4,7 +4,8 @@ class Solution {
         for(int[] a:dp){
             Arrays.fill(a, -1);
         }
-        return LCS(text1,text2,0,0,dp);
+        // return LCS(text1,text2,0,0,dp);
+        return LCSBU(text1,text2);
     }
     public static int LCS(String s1, String s2, int i, int j, int[][] dp){
         if(i== s1.length() || j== s2.length()){
@@ -23,5 +24,22 @@ class Solution {
             ans  = Math.max(f,s);
         }
         return dp[i][j]=ans;
+    }
+    public int LCSBU(String s1,String s2){
+        int ans=0;
+        int[][] dp = new int[s1.length()+1][s2.length()+1];
+        for(int i=1; i<dp.length; i++){
+            for(int j=1; j<dp[0].length; j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    int f = dp[i-1][j];
+                    int s = dp[i][j-1];
+                    dp[i][j]= Math.max(f,s);
+                }
+            }
+        }
+        return dp[dp.length-1][dp[0].length-1];
     }
 }
