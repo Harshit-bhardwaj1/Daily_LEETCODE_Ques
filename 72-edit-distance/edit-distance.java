da3ed68ad2464b882a3e5425ca53fcfdc2b2c3b3
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-10-11 23:28:34 +0530

    Time: 5 ms (58.86%) | Memory: 44.8 MB (88.34%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,7 +1,8 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         int[][] dp = new int[word1.length()][word2.length()];
-        return Min_Ops(word1,word2, 0,0,dp);
+        // return Min_Ops(word1,word2, 0,0,dp);
+        return Min_Ops_BU(word1,word2);
     }
     public static int Min_Ops(String s, String t , int i, int j,int[][] dp){
         if(i == s.length()){
@@ -25,5 +26,31 @@ class Solution {
             ans= Math.min(D, Math.min(R,I))+1;
         }
         return dp[i][j]=ans;
+    }
+
+    public static int Min_Ops_BU(String s, String t){
+    int[][] dp = new int[s.length()+1][t.length()+1];
+
+    for(int i =0; i<=t.length(); i++){
+        dp[0][i]=i;
+    }
+    for(int i=0; i<=s.length(); i++){
+        dp[i][0]=i;
+    }
+
+    for(int  i=1; i<=s.length(); i++){
+        for(int j=1; j<=t.length(); j++){
+            if(s.charAt(i-1)==t.charAt(j-1)){
+                dp[i][j]=dp[i-1][j-1];
+            }
+            else{
+                int D = dp[i-1][j];
+                int I = dp[i][j-1];
+                int R = dp[i-1][j-1];
+                dp[i][j] = Math.min(D, Math.min(I,R))+1;
+            }
+        }
+    }
+    return dp[s.length()][t.length()];
     }
 }
