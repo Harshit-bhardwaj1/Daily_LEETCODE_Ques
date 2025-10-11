5ac2c5a6c3ccd1e92d5b026d0fb66d0daa9db2e2
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-10-12 00:25:15 +0530

    Time: 6 ms (10.16%) | Memory: 47.5 MB (82.74%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -6,7 +6,8 @@ class Solution {
                 dp[i][j] = -1;
             }
         }
-        return pathSum(grid, 0, 0, dp);
+        // return pathSum(grid, 0, 0, dp);
+        return Path_Sum_TD(grid);
     }
     public static int pathSum(int[][] grid, int cr, int cc, int[][] dp) {
         if (cr == grid.length - 1 && cc == grid[0].length - 1) {
@@ -22,5 +23,27 @@ class Solution {
         int right = pathSum(grid, cr, cc + 1, dp);
         int down = pathSum(grid, cr + 1, cc, dp);
         return dp[cr][cc] = Math.min(right, down) + grid[cr][cc];
+    }
+    public static int Path_Sum_TD(int[][] grid){
+        int[][] dp = new int[grid.length][grid[0].length];
+
+        int sum=0;
+        for(int i=0; i<grid.length; i++){
+            sum+=grid[i][0];
+            dp[i][0]=sum;
+        }
+        sum=0;
+        for(int i=0; i<grid[0].length; i++){
+            sum+=grid[0][i];
+            dp[0][i]=sum;
+        }
+        for(int i=1; i<grid.length; i++){
+            for(int j=1; j<grid[0].length; j++){
+                int right = dp[i-1][j];
+                int down= dp[i][j-1];
+                dp[i][j]= Math.min(right,down)+grid[i][j];
+            }
+        }
+        return dp[grid.length-1][grid[0].length-1];
     }
 }
