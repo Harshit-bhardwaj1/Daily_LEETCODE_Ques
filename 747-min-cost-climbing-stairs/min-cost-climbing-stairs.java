e8e382d8e5789e6279cbdbd3e9dfbf5221322e15
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-10-16 23:51:54 +0530

    Time: 1 ms (37.11%) | Memory: 43.2 MB (50.81%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -20,45 +20,43 @@
 // }
 
 
+// class Solution{
+//     public int minCostClimbingStairs(int[] cost){
+//         int[] dp = new int[cost.length];
+//         Arrays.fill(dp,-1);
+//         int zero =Cost(cost,dp,0);
+//         int one =Cost(cost,dp,1);
+//         return Math.min(zero,one);
+//     }
+//     public int Cost(int[] cost,int[] dp, int idx){
+//         if(idx>=cost.length){
+//             return 0;
+//         }
+//         if(dp[idx]!=-1){
+//             return dp[idx];
+//         }
+//         int step1= Cost(cost,dp,idx+1);
+//         int step2= Cost(cost,dp,idx+2);
+//         return dp[idx]= cost[idx] + Math.min(step1,step2);
+//     }
+// }
+
+
 class Solution{
     public int minCostClimbingStairs(int[] cost){
-        int[] dp = new int[cost.length];
-        Arrays.fill(dp,-1);
-        int zero =Cost(cost,dp,0);
-        int one =Cost(cost,dp,1);
+        int zero =Cost_BU(cost,0);
+        int one =Cost_BU(cost,1);
         return Math.min(zero,one);
     }
-    public int Cost(int[] cost,int[] dp, int idx){
-        if(idx>=cost.length){
-            return 0;
+    public int Cost_BU(int [] cost,int idx){
+        int[] dp = new int[cost.length+1];
+        Arrays.fill(dp,0);
+        dp[cost.length-1]=cost[cost.length-1];
+        for(int i=cost.length-2; i>=0; i--){
+            int step1=cost[i]+dp[i+1];
+            int step2=cost[i]+dp[i+2];
+            dp[i]= Math.min(step1,step2);
         }
-        if(dp[idx]!=-1){
-            return dp[idx];
-        }
-        int step1= Cost(cost,dp,idx+1);
-        int step2= Cost(cost,dp,idx+2);
-        return dp[idx]= cost[idx] + Math.min(step1,step2);
-    }
-}
-
-
-class Solution1{
-    public int minCostClimbingStairs(int[] cost){
-        int[] dp = new int[cost.length];
-        Arrays.fill(dp,-1);
-        int zero= Cost(cost,dp,0);
-        int one= Cost(cost,dp,1);
-        return Math.min(zero, one);
-    }
-    public int Cost(int[] cost, int[] dp, int idx){
-        if(idx>=cost.length){
-            return 0;
-        }
-        if(dp[idx]!=-1){
-            return dp[idx];
-        }
-        int step1= Cost(cost,dp,idx+1);
-        int step2= Cost(cost,dp,idx+2);
-        return dp[idx]= cost[idx]+ Math.min(step1,step2);
+        return dp[idx];
     }
 }
