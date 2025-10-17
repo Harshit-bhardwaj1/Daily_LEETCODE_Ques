3ec20cb387e41f81bf027015471114918a6d1902
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-10-18 02:06:42 +0530

    Time: 21 ms (27.18%) | Memory: 58.4 MB (5.71%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -4,7 +4,8 @@ class Solution {
         for(int[] a: dp){
             Arrays.fill(a,-1);
         }
-        return Distinct(s,t,0,0,dp);
+        // return Distinct(s,t,0,0,dp);
+        return Distinct_BU(s,t);
     }
     public int Distinct(String s, String t, int i,int j, int[][] dp){
         if(t.length()==j){
@@ -22,5 +23,24 @@ class Solution {
         }
         int exc= Distinct(s,t,i+1,j,dp);
         return dp[i][j]= inc+exc;
+    }
+    public int Distinct_BU(String s, String t){
+        int[][] dp = new int[s.length()+1][t.length()+1];
+
+        for(int i=0; i<dp.length; i++){
+            dp[i][0]=1;
+        }
+
+        for(int i=1; i<dp.length; i++){
+            for(int j=1; j<dp[0].length; j++){
+                int inc=0;
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    inc = dp[i-1][j-1];
+                }
+                int exc = dp[i-1][j];
+                dp[i][j]= inc+exc;
+            }
+        }
+        return dp[s.length()][t.length()];
     }
 }
