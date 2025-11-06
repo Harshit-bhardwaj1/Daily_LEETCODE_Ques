fc42f2fedcb36299f12c2eb3ffec8cfa04d67e7c
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-11-06 22:27:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (5.13%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,8 +2,7 @@ class Solution {
     public int rob(int[] nums) {
         int[] dp = new int[nums.length];
         Arrays.fill(dp, -1);
-        // return Robber(nums,0,dp);
-        return Rob(nums,0,dp);
+        return Robber(nums,0,dp);
     }
     public static int Robber(int[] arr, int i, int[] dp){
         if(i>=arr.length){
@@ -16,15 +15,15 @@ class Solution {
         int Dont_rob=Robber(arr, i+1, dp);
         return dp[i]= Math.max(rob, Dont_rob);
     }
-    public int Rob(int[] arr,int i,int[] dp){
-        if(i>= arr.length){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int R = arr[i]+Rob(arr,i+2, dp);
-        int DR = Rob(arr,i+1, dp);
-        return dp[i]= Math.max(R,DR);
+    public static int RobberBU(int[] arr) {
+    int n = arr.length;
+    int[] dp = new int[n + 2]; // +2 so that i+2 access na ho out of bound
+    
+    for (int i = n - 1; i >= 0; i--) {
+        int rob = arr[i] + dp[i + 2];
+        int dontRob = dp[i + 1];
+        dp[i] = Math.max(rob, dontRob);
+    }
+    return dp[0];
     }
 }
