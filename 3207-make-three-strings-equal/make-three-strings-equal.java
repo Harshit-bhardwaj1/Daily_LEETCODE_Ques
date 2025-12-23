1e6691980c7e3c4d889fb23ffa9bd74b40d90988
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-12-23 18:25:57 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (17.84%) - LeetSync

---
+++ 3207-make-three-strings-equal/make-three-strings-equal.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int findMinimumOperations(String s1, String s2, String s3) {
+        return Find(s1,s2,s3);
+    }
+    public int Find(String s1, String s2, String s3){
+        int a= s1.length();
+        int b= s2.length();
+        int c = s3.length();
+        int n = Math.min(a,Math.min(b,c));
+        //int n1 = Math.min(s1.length(),s2.length(),s3.length());
+        if(s1.charAt(0)!=s2.charAt(0) || s1.charAt(0)!=s3.charAt(0) || s2.charAt(0)!=s3.charAt(0)) {
+            return -1;
+        }
+        int ans=0;
+        int count=1;
+        for(int i=1; i<n; i++){
+            char c1= s1.charAt(i);
+            char c2= s2.charAt(i);
+            char c3= s3.charAt(i);
+            if(c1!=c3 || c2!=c3 || c1!=c3){
+                break;
+            }
+            else{
+                count++;
+            }
+        }
+        ans+=(s1.length()-count)+(s2.length()-count)+(s3.length()-count);
+        return ans;
+    }
+}
