20490904900692703a3cce214f0e15c6d5ff2aa6
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2026-01-04 23:13:28 +0530

    Time: 1 ms (81.95%) | Memory: 43.4 MB (10.74%) - LeetSync

--- 1764-maximum-repeating-substring/maximum-repeating-substring.java
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.java
@@ -1,45 +1,49 @@
+class Solution {
+    public int maxRepeating(String sequence, String word) {
+        return Repeat(sequence, word);
+    }
+    public int Repeat(String sequence, String word){
+        String temp = word;
+        int k=0;
+        while(true){
+            if(sequence.contains(temp)){
+                k++;
+                temp+=word;
+            }
+            else{
+                break;
+            }
+        }
+        return k;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // class Solution {
 //     public int maxRepeating(String sequence, String word) {
-//         return Repeat(sequence, word);
-//     }
-//     public int Repeat(String sequence, String word){
-//         if(sequence.length()==1 && word.length()==1){
-//             if(sequence.charAt(0)== word.charAt(0)){
-//                 return 1;
-//             }
+//         int count = 0;
+//         String temp = word;
+
+//         while (sequence.contains(temp)) {
+//             count++;
+//             temp = temp + word;
 //         }
-//         int count =0;
-//         int i=0;
-//         int j=0;
-//         while(i<sequence.length()){
-//             if(sequence.charAt(i)==word.charAt(j)){
-//                 i++;
-//                 j++;
-//                 if(j==word.length()){
-//                     j=0;
-//                     count++;
-//                 }
-//             }
-//             else{
-//                 i++;
-//                 j=0;
-//             }
-//         }
+
 //         return count;
 //     }
 // }
-
-
-class Solution {
-    public int maxRepeating(String sequence, String word) {
-        int count = 0;
-        String temp = word;
-
-        while (sequence.contains(temp)) {
-            count++;
-            temp = temp + word;
-        }
-
-        return count;
-    }
-}
