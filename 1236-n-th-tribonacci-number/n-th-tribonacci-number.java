16a5a5bcb346bd83e2c593bf2bad7b615ff10c1a
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2026-01-06 12:55:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (13.86%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,15 +1,17 @@
 class Solution {
     public int tribonacci(int n) {
-        if (n == 0) return 0;
-        if (n == 1 || n == 2) return 1;
-
-        int t0 = 0, t1 = 1, t2 = 1;
-        for (int i = 3; i <= n; i++) {
-            int t3 = t0 + t1 + t2;
-            t0 = t1;
-            t1 = t2;
-            t2 = t3;
+        int t0=0;
+        int t1=1;
+        int t2=1;
+        int sum=0;
+        if(n==0) return 0;
+        if(n==1 || n==2) return 1;
+        for(int i=3; i<=n; i++){
+            sum=t0+t1+t2;
+            t0=t1;
+            t1=t2;
+            t2=sum;
         }
-        return t2;
+        return sum;
     }
 }
