f0eafc7979410643fc1ba1d41c27b0a168388266
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2026-01-19 23:09:44 +0530

    Time: 1 ms (27.70%) | Memory: 43.7 MB (8.54%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,6 +1,11 @@
 class Solution {
+    Boolean[][] dp;
     public boolean checkValidString(String s) {
-        return String(s);
+        int n = s.length();
+        dp = new Boolean[n + 1][n + 1];   // dp[i][p]
+        // int[][] dp = new int[]
+        // return String(s);
+        return Dp(s,0,0);
     }
     public boolean String(String s){
         int a=0;
@@ -24,5 +29,32 @@ class Solution {
         }
         
         return a==0;
+    }
+    public boolean Dp(String s, int i, int p){
+        if(p<0) return false;
+        if(i==s.length()){
+            return p==0;
+        }
+
+        if (dp[i][p] != null) {
+            return dp[i][p];             // already solved
+        }
+        boolean ans=false;
+        if(s.charAt(i)=='('){
+            ans = Dp(s,i+1,p+1);
+        }
+        else if(s.charAt(i)==')'){
+            ans = Dp(s,i+1,p-1);
+        }
+        else{
+            boolean a = Dp(s,i+1,p+1);
+            boolean b = Dp(s,i+1,p-1);
+            boolean c = Dp(s,i+1,p);
+
+            ans = a || b || c;
+        }
+
+        dp[i][p]=ans;
+        return ans;
     }
 }
