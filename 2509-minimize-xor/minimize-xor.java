beda17f34a3b4791a94e062edd4585f8353dc479
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2026-01-21 12:41:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (64.56%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minimizeXor(int num1, int num2) {
+        return Find_X(num1,num2);
+    }
+    public int Find_X(int num1, int num2){
+        int set_bit=0;
+        while(num2>0){
+            set_bit++;
+            num2=(num2 & (num2-1));
+        }
+
+
+        int x =0;
+        for(int i=30; i>=0; i--){
+            int mask = (1 << i);
+            if((num1 & mask) !=0){
+                x|=mask;  // x=x | mask
+                set_bit--;
+                if(set_bit==0){
+                    return x;
+                }
+            }   
+        }
+        for(int i=0; i<=30; i++){
+            int mask = (1<< i);
+            if((num1 & mask)==0){
+                x|=mask; 
+                set_bit--;
+                if(set_bit==0){
+                    return x;
+                }
+            }
+        }
+        return num1;
+    }
+}
