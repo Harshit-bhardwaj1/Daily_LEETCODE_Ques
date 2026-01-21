894ef4a38c0a94370233e08d13eaf1d350f67cd6
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2026-01-21 12:12:15 +0530

    Time: 1 ms (100.00%) | Memory: 78.9 MB (7.11%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.java
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.java
@@ -5,17 +5,17 @@ class Solution {
     public static int All_Pairing(int[] nums1, int[] nums2){
         int n = nums1.length;
         int m = nums2.length;
-        if(n%2==0 && m%2==0){
+        if(n%2==0 & m%2==0){
             return 0;
         }
         int xor1= Bitwise_XOR(nums1);
         int xor2= Bitwise_XOR(nums2);
 
-        if(n%2==1 && m%2==1){
-            return xor1 ^ xor2;
+        if(n%2==1 & m%2==1){
+            return xor1 ^ xor2; 
         }
-        else if(n%2==1){
-            return xor2;
+        else if(n%2==1){  // odd wale array ka even pair hoga inka xor 0 ho jayega
+            return xor2; // isiliye yaha even wale ka xor2 return kiya h
         }
         else{
             return xor1;
