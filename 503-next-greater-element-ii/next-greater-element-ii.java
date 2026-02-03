76e48c13c6d7da2591c4781c8df2ff0f0b28e499
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2026-02-04 01:57:30 +0530

    Time: 10 ms (75.73%) | Memory: 48.6 MB (54.17%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -7,26 +7,42 @@
 
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length;
-        int[] res = new int[n];
+        return Find(nums);
+        // int n = nums.length;
+        // int[] res = new int[n];
+        // Stack<Integer> st = new Stack<>();
+
+        // for (int i = 2 * n - 1; i >= 0; i--) {
+        //     int idx = i % n;
+
+        //     while (!st.isEmpty() && st.peek() <= nums[idx]) {
+        //         st.pop();
+        //     }
+
+        //     if (st.isEmpty()) {
+        //         res[idx] = -1;
+        //     } else {
+        //         res[idx] = st.peek();
+        //     }
+
+        //     st.push(nums[idx]);
+        // }
+
+        // return res;
+    }
+    public int[] Find(int [] nums){
         Stack<Integer> st = new Stack<>();
+        int[] ans = new int[nums.length];
+        Arrays.fill(ans,-1);
+        int n = nums.length;
+        for(int i=0; i<2*n; i++){
+            int num = nums[i%n];
 
-        for (int i = 2 * n - 1; i >= 0; i--) {
-            int idx = i % n;
-
-            while (!st.isEmpty() && st.peek() <= nums[idx]) {
-                st.pop();
+            while(!st.isEmpty() && num> nums[st.peek()]){
+                ans[st.pop()]=num;
             }
-
-            if (st.isEmpty()) {
-                res[idx] = -1;
-            } else {
-                res[idx] = st.peek();
-            }
-
-            st.push(nums[idx]);
+            if(i<n) st.push(i);
         }
-
-        return res;
+        return ans;
     }
 }
