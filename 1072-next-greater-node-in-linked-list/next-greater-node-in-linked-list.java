b50be594c1181c1f1587c8bd0ec39713b38fac67
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2026-02-04 01:38:26 +0530

    Time: 21 ms (75.62%) | Memory: 49.6 MB (73.23%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
@@ -10,28 +10,47 @@
  */
 class Solution {
     public int[] nextLargerNodes(ListNode head) {
-        ListNode temp = head;
-        int c=0;
-        List<Integer> arr = new ArrayList<>();
-        while(head!=null){
-            temp=head.next;
-            while(temp!=null){
-                if(temp.val>head.val){
-                    arr.add(temp.val);
-                    c=1;
-                    break;
-                }
-                temp=temp.next;
-            }
-            if(c==0){
-                arr.add(0);
-            }
-            else c=0;
-            head=head.next;
+        return Find(head);
+        // ListNode temp = head;
+        // int c=0;
+        // List<Integer> arr = new ArrayList<>();
+        // while(head!=null){
+        //     temp=head.next;
+        //     while(temp!=null){
+        //         if(temp.val>head.val){
+        //             arr.add(temp.val);
+        //             c=1;
+        //             break;
+        //         }
+        //         temp=temp.next;
+        //     }
+        //     if(c==0){
+        //         arr.add(0);
+        //     }
+        //     else c=0;
+        //     head=head.next;
+        // }
+        // int[] ans = new int[arr.size()];
+        // for(int i=0; i<arr.size(); i++){
+        //     ans[i]=arr.get(i);
+        // }
+        // return ans;
+    }
+    public int[] Find(ListNode head){
+        List<Integer> ll = new ArrayList<>();
+        ListNode temp= head;
+        while(temp!=null){
+            ll.add(temp.val);
+            temp=temp.next;
         }
-        int[] ans = new int[arr.size()];
-        for(int i=0; i<arr.size(); i++){
-            ans[i]=arr.get(i);
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[ll.size()];
+        for(int i=0; i<ll.size(); i++){
+
+            while(!st.isEmpty() && ll.get(i)>ll.get(st.peek())){
+                ans[st.pop()] = ll.get(i);
+            }
+            st.push(i);
         }
         return ans;
     }
