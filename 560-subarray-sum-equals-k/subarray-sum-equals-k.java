2bb466250d8744de3b490dbe58c515d1fc63e45b
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2026-03-17 16:16:32 +0530

    Time: 1537 ms (18.03%) | Memory: 48.7 MB (67.83%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -51,4 +51,20 @@ class Solution {
         }
         return count;
     }
+    public int Sum(int[] arr, int k){
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int[] prefix = new int[arr.length];
+        int sum=0;
+        int count=0;
+        for(int i=0; i<arr.length; i++){
+            sum+=arr[i];
+
+            if(map.containsKey(sum-k)){
+                count+=map.get(sum-k);
+            }
+            map.put(sum, map.getOrDefault(sum,0)+1);
+        }
+        return count;
+    }
 }
